1a3ddc8f7dd1def848002af393b95618fae4650f
Lexzee-IT <emeka.dike@outlook.com>
2025-03-18 11:05:25 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-  bucket = "my-dev-tf-state-bucket1"
+  bucket = "my-dev-tf-state-bucket001"
   key = "main"
   region = "us-east-1"
   dynamodb_table = "my-dynamodb-table"
